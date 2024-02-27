d046bb885e59ee74aafbe06e19922e46cee5284e
sahilg80 <38989332+sahilg80@users.noreply.github.com>
2024-02-27 17:42:23 +0530

    Added drag and drop functionality to monkey image

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IDropHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private Vector2 originalAnchoredPos;
+        private Vector3 originalRectPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -18,10 +20,38 @@ namespace ServiceLocator.UI
             this.owner = owner;
         }
 
+        public void OnDrag(PointerEventData eventData)
+        {
+            monkeyImage.rectTransform.anchoredPosition += eventData.delta;
+            owner.MonkeyDraggedAt(monkeyImage.rectTransform.position);
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            owner.MonkeyDroppedAt(eventData.position);
+            ResetMonkey();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.5f);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.rectTransform.anchoredPosition = originalAnchoredPos;
+            monkeyImage.rectTransform.position = originalRectPosition;
+            monkeyImage.color = new Color(1, 1, 1, 1);
+            monkeyImage.GetComponent<LayoutElement>().enabled = false;
+            monkeyImage.GetComponent<LayoutElement>().enabled = true;
+        }
+
         private void Awake()
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+            originalAnchoredPos = monkeyImage.rectTransform.anchoredPosition;
+            originalRectPosition = monkeyImage.rectTransform.position;
         }
     }
 }
